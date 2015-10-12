1a659d6278cd6e411de98b30ec2b23d9be9539fa
krishna <krishnaof1988@gmail.com>
2015-10-12 18:07:29 +0530

    TCP socket connection implemented

--- dns_example/src/scripts/safe_api/request_manager.js
+++ dns_example/src/scripts/safe_api/request_manager.js
@@ -2,13 +2,44 @@
 * Manage the Request
 * TODO MAID-1463
 */
-var RequestManager = function(encryptionKey, connectionManager) {
+var RequestManager = function(portNumber, launcherString, nonce, onReadyCallback) {
+  var connectionManager = require('./connection_manager');
+  var encryptionKey;
+  var self = this;
 
-  this.send = function(data, callback) {
-    // TODO Encrypt and send the data
-    // connectionManager.send
+  var onHandShakeComplete = function(err, handshakeResponse) {
+    if (err) {
+        onReadyCallback('Handshake failed with launcher - ' + err);
+        return;
+    }
+    // TODO get encryptionKey
+    onReadyCallback(null, self);
+  };
+
+  var handshake = function() {
+    // send Handshake request - MAID 1464
+  };
+
+
+  var onDataRecieved = function(data) {
+    // TODO disptach to callbacks of the Request
+  };
+
+  var onConnectionClosed = function() {
+    console.log('Launcher socket connection closed');
+  };
+
+  self.send = function(request, callback) {
+    // TODO Encrypt and send the request
+    // connectionManager.send(request);
   }
 
+  connectionManager.connect(port, handshake, {
+    'onData': onDataRecieved,
+    'onClosed': onConnectionClosed
+  });
+
+  return;
 };
 
-exports = module.exports = RequestManager();
+exports = module.exports = RequestManager;
