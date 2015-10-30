1a87b6692ff7122a6a7e9e79064ff2e70ace8c20
ustulation <ustulation@gmail.com>
2015-10-30 18:31:33 +0530

    nfs_example

--- nfs_example/src/main.rs
+++ nfs_example/src/main.rs
@@ -149,48 +149,79 @@ fn main() {
         arg_vec.push(it);
     }
 
-    // Authentication
+    if arg_vec.len() < 3 {
+        println!("App: Wrong number of command line agruments. This app is only meant to be started by Launcher.");
+        return
+    }
+
+    // --------------------------------------------------------------------------------
+    //                               Authentication
+    // --------------------------------------------------------------------------------
+    // Read the command line argument that Launcher starts this application with
     let main_arg = arg_vec[2].clone();
 
+    // Launcher given command line argument would read something like
+    // "tcp:127.0.0.1:403420:<Some-UTF-8-String>". So forming a regex to extract this information.
     let re = regex::Regex::new(r"^tcp:(.+?:\d+):(.+)$").unwrap();
     let captures = re.captures(&main_arg).unwrap();
 
+    // Get the Endpoint (e.g. 127.0.0.1:403420) Launcher is listening to.
     let ep = captures.at(1).unwrap().to_string();
+    // Get the random UTF-8 string that Launcher had passed.
     let str_nonce = captures.at(2).unwrap().to_string();
 
     println!("Ep {}, Nonce: {}", ep, str_nonce);
 
+    // Generate Key Pair and Nonce that Launcher will use to encrypt the Secret Key while passing
+    // it back to us.
+    // Generate Asymmetric Key-Pair using sodiumoxide.
     let (pub_key, priv_key) = sodiumoxide::crypto::box_::gen_keypair();
+    // Generate Nonce using sodiumoxide.
     let asym_nonce = sodiumoxide::crypto::box_::gen_nonce();
 
+    // Convert to Base64 encoded string so that we can put it into a JSON.
     let pub_key_b64 = pub_key.0.to_base64(get_base64_config());
     let asym_nonce_b64 = asym_nonce.0.to_base64(get_base64_config());
 
+    // Construct the Handshake data.
     let handshake_data = HandshakeData {
         launcher_string: str_nonce,
         asymm_nonce: asym_nonce_b64,
         asymm_pub_key: pub_key_b64,
     };
 
+    // Specify the correct endpoint - in this case authenticate-app.
     let handshake_json = HandshakeJson {
         endpoint: "safe-api/v1.0/handshake/authenticate-app".to_string(),
         data: handshake_data,
     };
 
+    // Encode it into a JSON.
     let json_str_handshake = rustc_serialize::json::encode(&handshake_json).unwrap();
 
+    // Connect to Launcher.
     let strm = std::net::TcpStream::connect(&ep[..]).ok().unwrap();
+    // Form an IpcStream to read and write to Launcher. This would help in reading from and writing
+    // to Launcher.
     let mut ipc_stream = stream::IpcStream::new(strm);
 
+    // Write our handshake request (Send it to Launcher)
     ipc_stream.write(json_str_handshake.into_bytes());
 
+    // Wait for Launcher's response
     let handshake_payload = ipc_stream.read_payload();
+    // Convert the raw bytes from stream to a valid UTF-8 String
     let payload_str = String::from_utf8(handshake_payload).ok().unwrap();
 
+    // Decode it into the expected Response structure - HandshakeResponse
     let handshake_response: HandshakeResponse = rustc_serialize::json::decode(&payload_str).ok().unwrap();
     println!("App: HandshakeResponse decoded");
 
+    // This is the encrypted symmetric key and nonce Launcher has passed us, duely encrypted
+    // with the Asymmetric keys we gave it earlier so that no one can snoop on it. Convert from
+    // base64 encoded String.
     let vec_encrypted_symm_key_nonce = handshake_response.data.encrypted_symm_key.from_base64().ok().unwrap();
+    // This is Launcher's Public Asymmetric Key - We will use this for decrypting the above.
     let vec_launcher_pub_key = handshake_response.data.launcher_public_key.from_base64().ok().unwrap();
     let mut launcher_pub_key = sodiumoxide::crypto::box_::PublicKey([0; sodiumoxide::crypto::box_::PUBLICKEYBYTES]);
 
@@ -200,6 +231,8 @@ fn main() {
         launcher_pub_key.0[it.0] = *it.1;
     }
 
+    // Finally decrypt using our Private Key, Nonce and Launcher's passed Public Key to get the
+    // secret key - this is a combination of secret nonce and symmetric key.
     let vec_decrypted_symm_key_nonce = sodiumoxide::crypto::box_::open(&vec_encrypted_symm_key_nonce,
                                                                        &asym_nonce,
                                                                        &launcher_pub_key,
@@ -210,6 +243,9 @@ fn main() {
     let mut symm_key = sodiumoxide::crypto::secretbox::Key([0; sodiumoxide::crypto::secretbox::KEYBYTES]);
     let mut symm_nonce = sodiumoxide::crypto::secretbox::Nonce([0; sodiumoxide::crypto::secretbox::NONCEBYTES]);
 
+    // Separate it into Secret Nonce and Symmetric Key - the secret key. Hence forth we will
+    // encrypt all data we send to Launcher using these and decrypt all data from Launcher using
+    // these.
     for it in vec_decrypted_symm_key_nonce.iter().take(sodiumoxide::crypto::secretbox::NONCEBYTES).enumerate() {
         symm_nonce.0[it.0] = *it.1;
     }
@@ -217,9 +253,12 @@ fn main() {
         symm_key.0[it.0] = *it.1;
     }
 
+    // --------------------------------------------------------------------------------
+    //                         Create a Directory - NFS operation
+    // --------------------------------------------------------------------------------
     println!("App: Begin creating directory...");
 
-    // Create a directory
+    // Fill in the details as in the RFC.
     let create_dir_data = CreateDirData {
         is_path_shared: false,
         dir_path: "/zeroeth".to_string(),
@@ -233,35 +272,53 @@ fn main() {
         data: create_dir_data,
     };
 
+    // Encode the request as a JSON.
     let create_dir_json_str = rustc_serialize::json::encode(&create_dir).unwrap_or_else(|a| panic!("{:?}", a));
     println!("App: CreateDir encoded");
 
+    // Get raw bytes to write to the stream to Launcher.
     let create_dir_bytes = create_dir_json_str.into_bytes();
 
+    // Encrypt the raw bytes using the Secret Key (Nonce and Symmetric Key).
     let create_dir_json_str_encrypted = sodiumoxide::crypto::secretbox::seal(&create_dir_bytes,
                                                                              &symm_nonce,
                                                                              &symm_key);
+    // Calculate the SHA512 of the above. This is the id that Launcher will give back so that we
+    // know to which of our requests does the response tie with.
     let response_id = sodiumoxide::crypto::hash::sha512::hash(&create_dir_json_str_encrypted);
 
+    // Send the encrypted data above to Launcher
     ipc_stream.write(create_dir_json_str_encrypted);
 
+    // Wait for Launcher response.
     let payload = ipc_stream.read_payload();
+    // Decrypt using the Secret Key.
     let payload_decrypted = sodiumoxide::crypto::secretbox::open(&payload,
                                                                  &symm_nonce,
                                                                  &symm_key).unwrap_or_else(|a| panic!("{:?}", a));
 
+    // Convert it to a UTF-8 String - this is the JSON response that Launcher has given us.
     let payload_str = String::from_utf8(payload_decrypted).unwrap_or_else(|a| panic!("{:?}", a));
 
+    // Decode the JSON to an expected response structure.
     let response: GenericResponse = rustc_serialize::json::decode(&payload_str).ok().unwrap();
     println!("App: GenericResponse decoded");
 
+    // Extract the response id from the Launcher response.
     let rxd_id_vec = response.id.from_base64().ok().unwrap();
 
+    // Check it is equal to the id that we had calculated. This confirms that this response is
+    // indeed a response to our request of Directory Creation.
     assert!(slice_equal(&rxd_id_vec, &response_id.0[..]));
 
     println!("App: Response from Launcher: {:?}", response.error);
 
-    // Fetch the created directory
+    // --------------------------------------------------------------------------------
+    //                         Create a Directory - NFS operation
+    // --------------------------------------------------------------------------------
+    println!("App: Begin fetching directory...");
+
+    // Form the Request structure - this is exactly as in Launcher RFC.
     let get_dir_data = GetDirData {
         timeout_ms: 0,
         is_path_shared: false,
@@ -273,33 +330,47 @@ fn main() {
         data: get_dir_data,
     };
 
+    // Encode the request as a JSON
     let json_str = rustc_serialize::json::encode(&get_dir_req).ok().unwrap();
     println!("App: GetDir encoded");
 
+    // Get raw bytes out of it.
     let json_str_bytes = json_str.into_bytes();
+    // Encrypt the raw bytes using the secret key.
     let encrypted_json_str_bytes = sodiumoxide::crypto::secretbox::seal(&json_str_bytes,
                                                                         &symm_nonce,
                                                                         &symm_key);
 
+    // Calculate and store the id which we will expect as response id given by Launcher for this
+    // request - similary to what we had done earlier.
     let response_id = sodiumoxide::crypto::hash::sha512::hash(&encrypted_json_str_bytes);
+    // Send encrypted JSON request to Launcher.
     ipc_stream.write(encrypted_json_str_bytes);
 
+    // Wait for Launcher's response
     let response = ipc_stream.read_payload();
+    // Decrypt Launcher's response - just like before.
     let decrypted_response = sodiumoxide::crypto::secretbox::open(&response,
                                                                   &symm_nonce,
                                                                   &symm_key).ok().unwrap();
     println!("App: GetDir Response decrypted.");
 
+    // Get it into a valid UTF-8 String - this will be the JSON response.
     let decrypted_response_json_str = String::from_utf8(decrypted_response).ok().unwrap();
     println!("App: GetDir Response JSON: {}", decrypted_response_json_str);
 
+    // Decode the JSON into expected response structure - in this case a directory response as
+    // stated in the RFC.
     let get_dir_response: GetDirResponse = rustc_serialize::json::decode(&decrypted_response_json_str)
                                                                  .unwrap_or_else(|e| panic!("{:?}", e));
     println!("App: GetDir Response decoded.");
 
+    // Extract the response id given by Launcher.
     let rxd_id_vec = get_dir_response.id.from_base64().ok().unwrap();
+    // Check this against the id that we had calculated earlier - should be same.
     assert!(slice_equal(&rxd_id_vec, &response_id.0[..]));
 
+    // Print the directory information.
     println!("App: Response: {:?}", get_dir_response);
     println!("App: Exiting test app...");
     println!("\n======================================================");
