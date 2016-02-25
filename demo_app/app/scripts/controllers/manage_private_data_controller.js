d0f86c49de5acddf542f93f6a6dd8393f75e1782
shankar2105 <shankar21mail@gmail.com>
2016-02-25 12:19:33 +0530

    code style updated

--- demo_app/app/scripts/controllers/manage_private_data_controller.js
+++ demo_app/app/scripts/controllers/manage_private_data_controller.js
@@ -1,14 +1,14 @@
 /**
  * Manage private data controller
  */
-window.maidsafeDemo.controller('PrivateDataCtrl', [ '$scope', '$timeout', 'safeApiFactory',
-function($scope, $timeout, safe) {
-  'use strict';
-  var PROGRESS_DELAY = 500;
-  $scope.progressIndicator = null;
-  $scope.registerProgress = function(progressScope) {
-    $scope.progressIndicator = progressScope;
-  };
+window.maidsafeDemo.controller('PrivateDataCtrl', ['$scope', '$timeout', 'safeApiFactory',
+  function($scope, $timeout, safe) {
+    'use strict';
+    var PROGRESS_DELAY = 500;
+    $scope.progressIndicator = null;
+    $scope.registerProgress = function(progressScope) {
+      $scope.progressIndicator = progressScope;
+    };
     // set target folder
     $scope.setTargetFolder = function(path) {
       // $scope.newServicePath = path;
