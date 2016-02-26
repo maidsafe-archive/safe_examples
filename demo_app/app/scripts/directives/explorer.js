e0fc0ebc4dae27816e03d2f0f11974d77ff6ab4d
shankar2105 <shankar21mail@gmail.com>
2016-02-26 12:14:07 +0530

    delete button Focus out removed on invalid places

--- demo_app/app/scripts/directives/explorer.js
+++ demo_app/app/scripts/directives/explorer.js
@@ -137,7 +137,6 @@ window.maidsafeDemo.directive('explorer', ['$rootScope', '$timeout', 'safeApiFac
 
       $scope.delete = function() {
         var path = $scope.currentDirectory + '/' + $scope.selectedPath;
-        $scope.listSelected = false;
         $rootScope.$loader.show();
         var onDelete = function(err) {
           $rootScope.$loader.hide();
@@ -180,7 +179,6 @@ window.maidsafeDemo.directive('explorer', ['$rootScope', '$timeout', 'safeApiFac
       };
 
       $scope.back = function() {
-        $scope.listSelected = false;
         var tokens = $scope.currentDirectory.split('/');
         tokens.pop();
         tokens.pop();
