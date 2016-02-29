2737cd944b8107497c6a23dfb922e3ec9eff4f0b
krishna <krishnaof1988@gmail.com>
2016-02-29 12:38:39 +0530

    Template public code clean up

--- demo_app/app/scripts/controllers/manage_private_data_controller.js
+++ demo_app/app/scripts/controllers/manage_private_data_controller.js
@@ -1,7 +1,7 @@
 /**
  * Manage private data controller
  */
-window.maidsafeDemo.controller('PrivateDataCtrl', [ '$scope', function($scope) {
+window.maidsafeDemo.controller('PrivateDataCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
     'use strict';
 
     $scope.onProgress = function(percentage, isUpload) {
--- demo_app/app/scripts/controllers/sample_template_controller.js
+++ demo_app/app/scripts/controllers/sample_template_controller.js
@@ -1,20 +1,29 @@
 /**
  * Sample site controller
  */
-window.maidsafeDemo.controller('SampleTemplateCtrl', [ '$scope', '$http', '$state', '$rootScope', 'safeApiFactory',
-function($scope, $http, $state, $rootScope, safe) {
-  'use strict';
-  $scope.siteTitle = 'My Page';
-  $scope.siteDesc = 'This page is created and published on the SAFE Network using the MaidSafe demo app';
-  var filePath = '/views/sample_template_layout.html';
-  var writeFile = function(title, content, filePath) {
-    $rootScope.$loader.show();
-    window.uiUtils.createTemplateFile(title, content, filePath, function(err, tempPath) {
+window.maidsafeDemo.controller('SampleTemplateCtrl', ['$scope', '$http', '$state', '$rootScope', 'safeApiFactory',
+  function($scope, $http, $state, $rootScope, safe) {
+    'use strict';
+    $scope.siteTitle = 'My Page';
+    $scope.siteDesc = 'This page is created and published on the SAFE Network using the MaidSafe demo app';
+    var filePath = '/views/sample_template_layout.html';
+
+    var onServiceCreated = function(err) {
+      var goToManageService = function() {
+        $state.go('manageService');
+      };
+      $rootScope.$loader.hide();
+      if (err) {
+        return $rootScope.prompt.show('Publish Service Error', err, goToManageService);
+      }
+      var msg = 'Template has been published for the service: ' + $state.params.serviceName;
+      $rootScope.prompt.show('Service Published', msg, goToManageService);
+    }
+
+    var onTemplateReady = function(err, tempPath) {
       if (err) {
         return console.error(err);
       }
-      var msg = null;
-      var path = require('path');
       var serviceName = $state.params.serviceName;
       var uploader = new window.uiUtils.Uploader(safe);
       var progress = uploader.upload(tempPath, false, '/public/' + serviceName);
@@ -24,35 +33,29 @@ function($scope, $http, $state, $rootScope, safe) {
         }
         if (progress.total === (progress.completed + progress.failed)) {
           $rootScope.$loader.show();
-          safe.addService(safe.getUserLongName(), serviceName, false, '/public/' + serviceName, function(err) {
-            $rootScope.$loader.hide();
-            if (err) {
-              msg = err;
-              return $rootScope.prompt.show('Publish Service Error', msg, function() {                
-                $state.go('manageService');
-              });
-            }
-            msg = 'Template has been published for the service: ' + serviceName;
-            $rootScope.prompt.show('Service Published', msg, function() {              
-              $state.go('manageService');
-            });
-          });
+          safe.addService(safe.getUserLongName(), serviceName, false, '/public/' + serviceName, onServiceCreated);
         }
       };
-    });
-  };
+    };
 
-  $scope.registerProgress = function(progressScope) {
-    $scope.progressIndicator = progressScope;
-  };
+    var writeFile = function(title, content, filePath) {
+      $rootScope.$loader.show();
+      window.uiUtils.createTemplateFile(title, content, filePath, onTemplateReady);
+    };
 
-  $scope.publish = function() {
-    console.log($scope.siteTitle + ' ' + $scope.siteDesc);
-    writeFile($scope.siteTitle, $scope.siteDesc, filePath);
-  };
 
-  $scope.handleInputClick = function(e) {
-    e.stopPropagation();
-    e.target.select();
-  };
-} ]);
+    $scope.registerProgress = function(progressScope) {
+      $scope.progressIndicator = progressScope;
+    };
+
+    $scope.publish = function() {
+      console.log($scope.siteTitle + ' ' + $scope.siteDesc);
+      writeFile($scope.siteTitle, $scope.siteDesc, filePath);
+    };
+
+    $scope.handleInputClick = function(e) {
+      e.stopPropagation();
+      e.target.select();
+    };
+  }
+]);
