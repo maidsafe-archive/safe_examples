fbfb2e01be18299fc7ac239e9560b2235a6d3487
krishna <krishnaof1988@gmail.com>
2016-06-30 11:30:31 +0530

    fix/file_upload: resolve file upload error handling
    Handled error callback on streaming upload failure

--- demo_app/app/scripts/factories/nfs_factory.js
+++ demo_app/app/scripts/factories/nfs_factory.js
@@ -101,8 +101,26 @@ window.maidsafeDemo.factory('nfsFactory', [ function(Shared) {
       auth: {
         'bearer': self.getAuthToken()
       }
-    }, function(err) {
-      callback(err);
+    }, function(e, response) {
+      if (response.statusCode !== 200) {
+        var errMsg = response.body;
+        if (!response.statusCode) {
+          errMsg = {
+            errorCode: 400,
+            description: 'Request connection closed abruptly'
+          }
+        } else {
+          try {
+            errMsg = JSON.parse(errMsg);
+          } catch(e) {
+            errMsg = {
+              errorCode: 400,
+              description: errMsg
+            }
+          }
+        }
+        callback({data: !response.statusCode ? 'Request connection closed' : JSON.parse(response.body)});
+      }
     }));
   };
 
