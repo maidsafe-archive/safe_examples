c7b011627b231532c755b922b920e1238561b657
Benjamin Kampmann <ben@create-build-execute.com>
2016-10-20 12:26:20 +0300

    chore/webrtc: Update to latest safe-js

--- webrtc_app/src/store.js
+++ webrtc_app/src/store.js
@@ -35,20 +35,32 @@ export function readData (item) {
   // we prefix everything with the app-id
   const address = btoa(`${APP_ID}-${item}`)
   // acquire handle
-  return safeStructuredData.getHandle(ACCESS_TOKEN, address)
+  console.log('reading', address)
+  return safeDataId.getStructuredDataHandle(ACCESS_TOKEN, address, 500)
     .then(res => res.__parsedResponseBody__ || res)
-    .then((resp) => {
-      const handleId = resp.handleId
-      // read handle
-      return safeStructuredData(ACCESS_TOKEN, handleId)
-        .then((payload) =>
-          // close handle
-          safeStructuredData.dropHandle(ACCESS_TOKEN, handleId)
-            // return read data
-            .then(() => JSON.parse(btoa(payload)))
-        )
+    .then(resp => {
+      const dataHandleId = resp.handleId
+      console.log(`${address}: acquired dataHandle ${dataHandleId}`)
+      return safeStructuredData.getHandle(ACCESS_TOKEN, dataHandleId)
+        .then(res => res.__parsedResponseBody__ || res)
+        .then((resp) => {
+          const handleId = resp.handleId
+          console.log(`${address}: acquired handle ${handleId}`)
+          // read handle
+          return safeDataId.dropHandle(ACCESS_TOKEN, dataHandleId).then(
+            r => safeStructuredData.readData(ACCESS_TOKEN, handleId)
+              .then((payload) => {
+                console.log(`${address}: read data: ${payload}`)
+                // close handle
+                safeStructuredData.dropHandle(ACCESS_TOKEN, handleId)
+                // return read data
+                return JSON.parse(payload)
+              })
+            )
+        }
+      )
     }
-    )
+  )
 }
 
 export function publishData (name, payload) {
@@ -56,7 +68,7 @@ export function publishData (name, payload) {
   // we prefix everything with the app-id
   const address = btoa(`${APP_ID}-${name}`)
   const data = new Buffer(JSON.stringify(payload)).toString('base64')
-  console.log('putting', address, data)
+  console.log('putting', address)
   return safeStructuredData.create(ACCESS_TOKEN, address, 500, data)
     .then(res => res.__parsedResponseBody__ || res)
     .then(res => {
@@ -64,6 +76,6 @@ export function publishData (name, payload) {
       // post data creates the actual structure
       return safeStructuredData.put(ACCESS_TOKEN, handleId)
         // drop the handle
-        .then(p => safeStructuredData.dropHandle(handleId))
+        // .then(p => safeStructuredData.dropHandle(handleId))
     })
 }
