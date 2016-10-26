d7bdd0a5d310271b1245e6a24b72f8c8cc560bf6
Benjamin Kampmann <ben@create-build-execute.com>
2016-10-26 17:41:14 +0300

    feat/versioning_editor: Load and show file index

--- versioning_editor/src/App.js
+++ versioning_editor/src/App.js
@@ -3,7 +3,7 @@ import Editor from 'react-md-editor'
 import './App.css'
 
 import { EDITOR_THEME } from './config.js'
-import { authorise } from './store.js'
+import { authorise, refreshFileIndex } from './store.js'
 
 
 require("../node_modules/codemirror/lib/codemirror.css")
@@ -43,12 +43,37 @@ Some **bold** and _italic_ text
 
   render() {
     return (
+      <div>
+        <h1>Editing <em>{this.props.filename}</em></h1>
         <Editor
           value={this.state.code}
           options={this.editorOpts}
           onChange={this.updateCode} />
+      </div>
     )
   }
+}
+
+class FileSelector extends Component {
+
+  newFile() {
+    console.log(this.refs.filename)
+    let filename = this.refs.filename.value.trim()
+    if (!filename) return
+    this.props.onSelectFile(filename)
+  }
+
+  render() {
+    return <div>
+      <h1>Select the file you want to edit</h1>
+      <ul>
+        <li></li>
+      </ul>
+
+      <div><input ref="filename" placeholder="new filename here" /> <button onClick={this.newFile.bind(this)}>+ new File</button></div>
+    </div>
+  }
+
 }
 
 class App extends Component {
@@ -56,21 +81,30 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      'authorised': false
+      'authorised': false,
+      'selectedFile': null,
+      'files': null
     }
   }
 
   componentWillMount() {
     authorise().then(() => {
       this.setState({'authorised': true})
+      return refreshFileIndex().then(files => {
+        this.setState({'files': files})
+      })
     })
   }
 
   render() {
 
     let sub = <div className="info"><p>Please authorise the app in Launcher.</p></div>
-    if (this.state.authorised){
-      sub = <ManagedEditor />
+    if (this.state.authorised) {
+      if (this.state.selectedFile) {
+        sub = <ManagedEditor filename={this.state.selectedFile} />
+      } else {
+        sub = <FileSelector onSelectFile={(f) => this.setState({selectedFile: f})} />
+      }
     }
     return (
       <div className="App">
--- versioning_editor/src/store.js
+++ versioning_editor/src/store.js
@@ -1,4 +1,4 @@
-/* global btoa, safeAuth, safeStructuredData, safeDataId */
+/* global btoa, safeAuth, safeCipherOpts, safeStructuredData, safeDataId */
 
 // this is only file directly interacting with SAFE
 
@@ -8,8 +8,23 @@ if (process.env.NODE_ENV !== 'production') {
   require('safe-js/dist/polyfill')
 }
 
-// global access token
+const INDEX_FILE_NAME = btoa(`${APP_ID}+index`)
+
+// global access state
 let ACCESS_TOKEN
+let SYMETRIC_CYPHER_HANDLE
+let INDEX_HANDLE
+let FILE_INDEX = {}
+
+function _refreshCypherHandle(){
+  return safeCipherOpts.getHandle(ACCESS_TOKEN,
+          window.safeCipherOpts.getEncryptionTypes().SYMMETRIC)
+    .then(res => {
+      SYMETRIC_CYPHER_HANDLE = res.handleId || res.__parsedResponseBody__.handleId
+      return SYMETRIC_CYPHER_HANDLE
+    }
+  )
+}
 
 export function authorise () {
   if (ACCESS_TOKEN) return Promise.resolve(ACCESS_TOKEN)
@@ -22,7 +37,7 @@ export function authorise () {
     'permissions': ['LOW_LEVEL_API']
   },
       APP_ID)
-    .then(res => res.__parsedResponseBody__ || res)
+    .then(res => res.__parsedResponseBody__ || res) // legacy style fallback
     .then(auth => auth.token === APP_ID ? safeAuth.getAuthToken(APP_ID) : auth.token)
     .then(token => {
       if (!token) {
@@ -30,7 +45,59 @@ export function authorise () {
         throw Error("Authentication Failed")
       }
       ACCESS_TOKEN = token
-      return ACCESS_TOKEN
+      // then fetch a fresh cypherHandle
+      return _refreshCypherHandle().then(() => ACCESS_TOKEN)
     }
   )
 }
+
+// legacy style fallback
+const extractHandle = (res) => res.handleId || res.__parsedResponseBody__.handleId
+
+
+function _putFileIndex () {
+  return safeStructuredData.updateData(ACCESS_TOKEN,
+    INDEX_HANDLE,
+    new Buffer(JSON.stringify(FILE_INDEX)).toString('base64'),
+    SYMETRIC_CYPHER_HANDLE)
+}
+
+export function refreshFileIndex () {
+  return safeDataId.getStructuredDataHandle(ACCESS_TOKEN, INDEX_FILE_NAME, 500)
+    .then(extractHandle) 
+    .then(dataHandle => {
+      return safeStructuredData.getHandle(ACCESS_TOKEN, dataHandle)
+        // or create!
+        .catch((e) => {
+          console.error(e)
+          return safeStructuredData.create(
+              ACCESS_TOKEN, INDEX_FILE_NAME, 500,
+              new Buffer(JSON.stringify({})).toString('base64'),
+              SYMETRIC_CYPHER_HANDLE)
+            .then(extractHandle)
+            .then(handle => safeStructuredData.put(ACCESS_TOKEN, handle)
+              // don't forget to clean up that handle
+              .then(() => safeStructuredData.dropHandle(handle))
+            // and return a read handle
+            .then(() => safeStructuredData.getHandle(ACCESS_TOKEN, dataHandle)))
+        })
+        .then(extractHandle)
+        // then clean up the dataHandle
+        .then(sdHandle => safeDataId.dropHandle(dataHandle).then(() => sdHandle))
+        .then(sdHandle => {
+          INDEX_HANDLE = sdHandle
+          return sdHandle
+        })
+    }).then(sdHandle => 
+      // try to read
+      safeStructuredData.readData(ACCESS_TOKEN, sdHandle)
+        .then(payload => {
+          FILE_INDEX = payload
+        })
+    ).then(() => FILE_INDEX)
+}
+
+
+
+
+
