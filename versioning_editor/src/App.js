c3b14a53856c26f006ad40d1b165567eb1128ea9
Benjamin Kampmann <ben@create-build-execute.com>
2016-10-26 15:45:10 +0300

    feat/versioning_editor: Add React-MD-Editor setup

--- versioning_editor/src/App.js
+++ versioning_editor/src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Editor from 'react-md-editor';
 import './App.css';
 
+// CONFIGURATION
+
+// Theme for the editor
+// try 'elegant' or 'material'
+// see all: https://codemirror.net/demo/theme.html
+const EDITOR_THEME = 'mdn-like'; 
+
+
+require("../node_modules/codemirror/lib/codemirror.css")
+require('../node_modules/codemirror/theme/'+ EDITOR_THEME + '.css')
+require("../node_modules/react-md-editor/dist/react-md-editor.css")
+
+
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.editorOpts = {
+      'theme': EDITOR_THEME
+    }
+
+    this.state = {
+      code: `# React Markdown Editor
+
+* A list
+* with
+* some items
+
+Some **bold** and _italic_ text
+
+> A quote...`
+    }
+    this.updateCode = this.updateCode.bind(this)
+
+  }
+
+  updateCode(newCode) {
+    this.setState({
+        code: newCode
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to SAFE example Editor</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Editor
+          value={this.state.code}
+          options={this.editorOpts}
+          onChange={this.updateCode} />
       </div>
     );
   }
