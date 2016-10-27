66d8961c758548d787f863d5cad15fb89f9600e0
Benjamin Kampmann <ben@create-build-execute.com>
2016-10-27 11:33:40 +0300

    feat/versioning_editor : use NFS to store permanent app config prefix

--- versioning_editor/src/config.js
+++ versioning_editor/src/config.js
@@ -10,6 +10,6 @@ export const EDITOR_THEME = 'mdn-like'
 
 // GLOBAL CONSTANTS
 export const APP_NAME = "Example Editor"
-export const APP_VERSION = '0.1'
-export const APP_ID = 'example.editor.v' + APP_VERSION
+export const APP_VERSION = '0.2'
+export const APP_ID = 'net.maidsafe.examples.versioning-editor'
 
--- versioning_editor/src/store.js
+++ versioning_editor/src/store.js
@@ -1,4 +1,4 @@
-/* global btoa, safeAuth, safeCipherOpts, safeStructuredData, safeDataId */
+/* global btoa, safeAuth, safeNFS safeCipherOpts, safeStructuredData, safeDataId */
 
 // this is only file directly interacting with SAFE
 
@@ -8,15 +8,23 @@ if (process.env.NODE_ENV !== 'production') {
   require('safe-js/dist/polyfill')
 }
 
-const INDEX_FILE_NAME = btoa(`${APP_ID}+index`)
-
 // global access state
 let ACCESS_TOKEN
 let SYMETRIC_CYPHER_HANDLE
 let INDEX_HANDLE
 let FILE_INDEX
+let USER_PREFIX
 
-function _refreshCypherHandle(){
+function _createRandomUserPrefix() {
+  let randomString = ''
+  for (var i = 0; i < 10; i++) {
+    // and ten random ascii chars
+    randomString += String.fromCharCode(Math.floor(Math.random(100)))
+  }
+  return btoa(`${APP_ID}@${APP_VERSION}#${(new Date()).getTime()}-${randomString}`)
+}
+
+function _refreshCypherHandle () {
   return safeCipherOpts.getHandle(ACCESS_TOKEN,
           window.safeCipherOpts.getEncryptionTypes().SYMMETRIC)
     .then(res => {
@@ -24,6 +32,22 @@ function _refreshCypherHandle(){
       return SYMETRIC_CYPHER_HANDLE
     }
   )
+}
+
+function _refreshConfig() {
+  // reading the config from NFS or create it if not yet existing.
+  const FILE_NAME = 'app_config.json'
+  return safeNFS.createFile(ACCESS_TOKEN,
+        // try to create instead then
+        FILE_NAME,
+        `{"user_prefix": "${_createRandomUserPrefix()}" }`)
+    .catch(err => err.errorCode === -505 ? '' : console.error(err)) // ignore file already exists
+    .then(safeNFS.getFile(ACCESS_TOKEN, FILE_NAME)
+      .then(resp => resp.json ? resp.json() : resp.body().then(JSON.parse))
+      .then(config => {
+        USER_PREFIX = config.user_prefix
+      })  
+    )
 }
 
 export function authorise () {
@@ -34,7 +58,7 @@ export function authorise () {
     'id': APP_ID,
     'version': APP_VERSION,
     'vendor': 'MaidSafe Ltd.',
-    'permissions': ['LOW_LEVEL_API']
+    'permissions': ['LOW_LEVEL_API', 'SAFE_DRIVE_ACCESS']
   },
       APP_ID)
     .then(res => res.__parsedResponseBody__ || res) // legacy style fallback
@@ -46,7 +70,10 @@ export function authorise () {
       }
       ACCESS_TOKEN = token
       // then fetch a fresh cypherHandle
-      return _refreshCypherHandle().then(() => ACCESS_TOKEN)
+      return Promise.all([
+          _refreshCypherHandle(),
+          _refreshConfig()
+        ]).then(() => ACCESS_TOKEN)
     }
   )
 }
@@ -74,8 +101,10 @@ export function saveFile(filename, data) {
   } else {
     // file is being created for the first time
     return safeStructuredData.create(ACCESS_TOKEN,
-        `${APP_ID}-${btoa(filename)}`, 501,
-        payload, SYMETRIC_CYPHER_HANDLE)
+        // trying to come up with a name that is super unlikely to clash ever.
+        btoa(`${USER_PREFIX}:${filename}`),
+        // 501 => we want this versioned
+        501, payload, SYMETRIC_CYPHER_HANDLE)
       .then(extractHandle)
       // save the structure
       .then(handle => safeStructuredData.put(ACCESS_TOKEN, handle)
@@ -92,40 +121,38 @@ export function saveFile(filename, data) {
 
 export function getFileIndex () {
   if (FILE_INDEX) return Promise.resolved(FILE_INDEX)
+  const INDEX_FILE_NAME = btoa(`${USER_PREFIX}#index`)
 
   return safeDataId.getStructuredDataHandle(ACCESS_TOKEN, INDEX_FILE_NAME, 500)
-    .then(extractHandle) 
-    .then(dataHandle => {
-      return safeStructuredData.getHandle(ACCESS_TOKEN, dataHandle)
-        // or create!
-        .catch((e) => {
-          FILE_INDEX = {}
-          return safeStructuredData.create(
-              ACCESS_TOKEN, INDEX_FILE_NAME, 500,
-              new Buffer(JSON.stringify({})).toString('base64'),
-              SYMETRIC_CYPHER_HANDLE)
-            .then(extractHandle)
-            .then(handle => safeStructuredData.put(ACCESS_TOKEN, handle)
-              // don't forget to clean up that handle
-              .then(() => safeStructuredData.dropHandle(handle))
-            // and return a read handle
-            .then(() => safeStructuredData.getHandle(ACCESS_TOKEN, dataHandle)))
-        })
-        .then(extractHandle)
-        // then clean up the dataHandle
-        // .then(sdHandle => safeDataId.dropHandle(dataHandle).then(() => sdHandle))
-        .then(sdHandle => {
-          INDEX_HANDLE = sdHandle
-          return sdHandle
-        })
-    }).then(sdHandle => 
-      // try to read
-      safeStructuredData.readData(ACCESS_TOKEN, sdHandle)
-        .then(resp => resp.json())
-        .then(payload => {
-          FILE_INDEX = payload
-        })
-    ).then(() => FILE_INDEX)
+    .then(extractHandle)
+    .then(handle => safeStructuredData.getHandle(ACCESS_TOKEN, handle)
+      .then(extractHandle)
+      // drop data Handle
+      // .then(sdHandle => safeDataId.dropHandle(handle).then(() => sdHandle))
+      .then(sdHandle => {
+        // store the handle for future reference
+        INDEX_HANDLE = sdHandle
+        // let's try to read
+        return safeStructuredData.readData(ACCESS_TOKEN, sdHandle, '')
+          .then(resp => resp.json ? resp.json() : resp.body())
+      },            
+      (e) => {
+        console.error(e)
+        FILE_INDEX = {}
+        return safeStructuredData.create(ACCESS_TOKEN, INDEX_FILE_NAME, 500,
+            new Buffer(JSON.stringify({})).toString('base64'), SYMETRIC_CYPHER_HANDLE)
+          .then(extractHandle)
+          .then(handle => safeStructuredData.put(ACCESS_TOKEN, handle)
+            // don't forget to clean up that handle
+            .then(() => safeStructuredData.dropHandle(handle)))
+          // and return empty data as payload
+          .then(() => { return {} })
+      }))
+    .then(payload => {
+      // store payload for future reference
+      FILE_INDEX = payload
+      return FILE_INDEX
+    })
 }
 
 
