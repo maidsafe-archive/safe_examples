57fc3b62e24270ee59a4730fc18179113853c6d2
krishna <krishnaof1988@gmail.com>
2016-11-15 22:17:44 +0530

    version change of comments plugin

--- editable_comments_plugin/comments/src/model.js
+++ editable_comments_plugin/comments/src/model.js
@@ -12,7 +12,7 @@
       this.appInfo = {
         name: window.location.host,
         id: 'tutorial.maidsafe.net',
-        version: '0.1.0',
+        version: '0.1.1',
         vendor: 'maidsafe',
         permissions: [
           'LOW_LEVEL_API'
