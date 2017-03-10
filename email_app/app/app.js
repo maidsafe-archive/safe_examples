76fabc8eb7733b2a1748fcb1183325e4ab5ffe15
Benjamin Kampmann <ben@gnunicorn.org>
2017-03-10 14:59:48 +0100

    fix/Email Switch to latest electron forge setup

---
+++ email_app/app/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import routes from './routes';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+const history = syncHistoryWithStore(hashHistory, store);
+
+export default class App extends React.Component {
+  render() {
+    return (<Provider store={store}>
+			    <Router history={history} routes={routes(store)} />
+			  </Provider>);
+	}
+}
