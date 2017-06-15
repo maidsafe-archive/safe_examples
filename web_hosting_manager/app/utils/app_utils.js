d4002a13d4c45d532ec8396f179d2d615d7ed25e
Shankar <shankar21mail@gmail.com>
2017-06-15 14:53:27 +0530

    fix/tagtype_issue: resolve tagtype issue (#188)
    Resolve tagtype issue of service Data.
    File update after deleted resolved.
    Error description UI issue fixed.

--- web_hosting_manager/app/utils/app_utils.js
+++ web_hosting_manager/app/utils/app_utils.js
@@ -1,5 +1,5 @@
 export const trimErrorMsg = (msg) => {
   let index = msg.indexOf('->');
-  index = (index === -1) ? index : index + 2;
+  index = (index === -1) ? 0 : index + 2;
   return msg.slice(index).trim()
 };
