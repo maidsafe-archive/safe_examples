f3fb874e6346113ba2dbbdae44073c71ea64cb7e
Shankar <shankar21mail@gmail.com>
2017-07-12 12:15:43 +0530

    select email UI integrated

--- email_app/app/components/create_account.js
+++ email_app/app/components/create_account.js
@@ -14,8 +14,8 @@ export default class CreateAccount extends Component {
   storeCreatedAccount() {
     const { newAccount, storeNewAccount, createAccountError } = this.props;
     return storeNewAccount(newAccount)
-        .then((_) => this.context.router.push('/home'))
-        .catch((e) => createAccountError(new Error(e)));
+      .then((_) => this.context.router.push('/home'))
+      .catch((e) => createAccountError(new Error(e)));
   }
 
   handleCreateAccount(e) {
@@ -31,45 +31,90 @@ export default class CreateAccount extends Component {
     }
 
     return createAccount(emailId)
-        .then(this.storeCreatedAccount)
-        .catch((err) => {
-          if (err.code === SAFE_APP_ERROR_CODES.ERR_DATA_EXISTS
-               || err.code === SAFE_APP_ERROR_CODES.ENTRY_ALREADY_EXISTS) {
-            return createAccountError(new Error(MESSAGES.EMAIL_ALREADY_TAKEN));
-          }
-          return createAccountError(err);
-        });
+      .then(this.storeCreatedAccount)
+      .catch((err) => {
+        if (err.code === SAFE_APP_ERROR_CODES.ERR_DATA_EXISTS
+          || err.code === SAFE_APP_ERROR_CODES.ENTRY_ALREADY_EXISTS) {
+          return createAccountError(new Error(MESSAGES.EMAIL_ALREADY_TAKEN));
+        }
+        return createAccountError(err);
+      });
   };
 
   handleChooseAccount(e) {
     e.preventDefault();
     const { refreshConfig, createAccountError } = this.props;
-    const emailId = this.emailId.value;
+    const emailId = this.emailSelected.value;
 
     return refreshConfig(emailId)
-        .then((_) => this.context.router.push('/home'))
-        .catch((e) => createAccountError(new Error(e)));
+      .then((_) => this.context.router.push('/home'))
+      .catch((e) => createAccountError(new Error(e)));
   };
 
   render() {
     console.log("Email IDs to choose from: ", this.props.email_ids);
+
+    const emailList = this.props.email_ids.length === 0 ? [ 'Email1', 'Email2', 'Email3' ] : this.props.email_ids;
 
     const { processing, error } = this.props;
     return (
       <div className="create-account">
         <div className="create-account-b">
           <div className="create-account-cnt text-center">
-            <h3 className="title">Create Email Id</h3>
-            <form className="form" onSubmit={this.handleCreateAccount}>
-              <div className="inp-grp">
-                <input type="text" name="emailId" id="emailId" ref={c => {this.emailId = c;}} autoFocus="autoFocus" required="required" />
-                <label htmlFor="emailId">Email ID</label>
-                <div className="alert">Email Id must be less than {CONSTANTS.EMAIL_ID_MAX_LENGTH} characters. (This is just a restriction in this tutorial)</div>
+            <div className="split-view">
+              <div className="split-view-i">
+                <div className="create-email">
+                  <h3 className="title">Create Email Id</h3>
+                  <form className="form" onSubmit={this.handleCreateAccount}>
+                    <div className="inp-grp">
+                      <input type="text" name="emailId" id="emailId" ref={c => {this.emailId = c;}} autoFocus="autoFocus" required="required" />
+                      <label htmlFor="emailId">Email ID</label>
+                      <div className="alert">Email Id must be less than {CONSTANTS.EMAIL_ID_MAX_LENGTH} characters. (This is just a restriction in this tutorial)</div>
+                    </div>
+                    <div className="inp-btn-cnt">
+                      <button type="submit" className="mdl-button mdl-js-button mdl-button--raised bg-primary" disabled={processing.state}>Create</button>
+                    </div>
+                  </form>
+                </div>
               </div>
-              <div className="inp-btn-cnt">
-                <button type="submit" className="mdl-button mdl-js-button mdl-button--raised bg-primary" disabled={processing.state}>Create</button>
+              <div className="split-view-i">
+                <div className="email-ls">
+                  <h3 className="title">Select Email Id</h3>
+                  <form className="form">
+                    <div className="inp-grp">
+                      <div
+                        className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select getmdl-select__fix-height">
+                        <input
+                          className="mdl-textfield__input"
+                          type="text"
+                          id="selectEmail"
+                          ref={(c) => { this.emailSelected = c;}}
+                          readOnly="readOnly"
+                          placeholder="Select email ID"
+                          tabIndex="-1"
+                          value={emailList[0]}
+                        />
+                        {
+                          emailList.length !== 0 ? (<ul htmlFor="selectEmail" className="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+                              { emailList.map((email, i) => {
+                                return (<li key={`email${i}`} className="mdl-menu__item">{email}</li>)
+                              }) }
+                            </ul>) : null
+                        }
+                      </div>
+                      <div className="inp-btn-cnt">
+                        <button
+                          type="submit"
+                          className="mdl-button mdl-js-button mdl-button--raised bg-primary"
+                          disabled={emailList.length === 0}
+                          onClick={this.handleChooseAccount}
+                        >Select</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </form>
+            </div>
             <h4 className="error">{error.message}</h4>
           </div>
         </div>
