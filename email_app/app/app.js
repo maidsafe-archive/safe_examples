0f9c8714bbd81098fb97bc431f24b155c4bdf693
hunterlester <guilfordhunter@gmail.com>
2017-08-18 14:35:10 -0700

    fetch log file added to email app help menu

--- email_app/app/app.js
+++ email_app/app/app.js
@@ -3,12 +3,23 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { ipcRenderer as ipc } from 'electron';
+import { ipcRenderer as ipc, remote, shell } from 'electron';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import { receiveResponse, onAuthFailure } from "./actions/initializer_actions";
+const { Menu, MenuItem } = remote;
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
+
+let menu = Menu.getApplicationMenu();
+menu.items[4].submenu.append(new MenuItem({ label: 'Error Logs', click() {
+  const state = store.getState();
+  state.initializer.app.logPath()
+  .then((path) => {
+    console.log('Log file located at: ', path);
+    shell.openExternal(path);
+  })
+} }))
 
 ipc.on('auth-response', (event, response) => {
   if (response && response.indexOf('safe-') == 0) {
