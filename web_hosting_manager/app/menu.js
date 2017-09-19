a197aa9da3ee304c05d4186712307dc4cac69eb4
Shankar <shankar21mail@gmail.com>
2017-09-19 15:06:24 +0530

    disable devtool on production env

--- web_hosting_manager/app/menu.js
+++ web_hosting_manager/app/menu.js
@@ -10,7 +10,7 @@ export default class MenuBuilder {
   }
 
   buildMenu() {
-    this.mainWindow.openDevTools();
+    // this.mainWindow.openDevTools();
     if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
       this.setupDevelopmentEnvironment();
     }
