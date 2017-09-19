0e680beb0e2dbf7dcef9ebe04107b1bcd89da98d
Shankar <shankar21mail@gmail.com>
2017-09-19 15:43:00 +0530

    Update Hash history on production mode

--- web_hosting_manager/app/store/configureStore.prod.js
+++ web_hosting_manager/app/store/configureStore.prod.js
@@ -2,11 +2,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(promiseMiddleware(), thunk, router);
 
