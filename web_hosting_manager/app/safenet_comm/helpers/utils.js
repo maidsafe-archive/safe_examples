62ecaeae73c6587edafeb751e3e5470ce41dfb67
Shankar <shankar21mail@gmail.com>
2017-11-30 18:01:05 +0530

    fix/path_separator: resolve path separator issue
    Resolved platform specific path separator issue.

--- web_hosting_manager/app/safenet_comm/helpers/utils.js
+++ web_hosting_manager/app/safenet_comm/helpers/utils.js
@@ -20,9 +20,10 @@ export const parseNetworkPath = (nwPath) => {
     file: undefined,
   };
   if (nwPath) {
-    const splitPath = nwPath.split('/');
-    result.dir = splitPath.slice(0, 3).join('/');
-    result.file = splitPath.slice(3).join('/') || path.basename(nwPath);
+    const sep = path.sep;
+    const splitPath = nwPath.split(sep);
+    result.dir = splitPath.slice(0, 3).join(sep);
+    result.file = splitPath.slice(3).join(sep) || path.basename(nwPath);
   }
   return result;
 };
