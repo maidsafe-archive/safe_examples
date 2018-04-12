5e2c3d97e3059ae21873dc7760d0a93c48a21de0
bochaco <gabrielviganotti@gmail.com>
2018-04-12 14:41:41 +0100

    fix/boilerplate: add electron listener needed for receiving the auth URI in OSX

--- safe_app_electron_quick_start/main.js
+++ safe_app_electron_quick_start/main.js
@@ -73,5 +73,9 @@ app.on('activate', function () {
   }
 })
 
+app.on('open-url', (e, resAuthUri) => {
+  mainWindow.webContents.send('system-uri-response', resAuthUri);
+})
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
