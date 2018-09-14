06963790965a4ccab7b86db6e6b118e8bd06655b
bochaco <gabrielviganotti@gmail.com>
2018-09-14 11:49:31 -0300

    chore/whm: remove WHM app since it was moved to itw own safe-web-hosting-manager-electron repo

--- web_hosting_manager/app/actions/action_types/common.js
+++
@@ -1,4 +0,0 @@
-export default {
-  RESET: 'RESET',
-  GET_LOG_FILE_PATH: 'GET_LOG_FILE_PATH',
-};
--- web_hosting_manager/app/safenet_comm/helpers/task.js
+++
@@ -1,8 +0,0 @@
-export default class Task {
-  /* eslint-disable class-methods-use-this */
-  execute(callback) {
-    /* eslint-enable class-methods-use-this */
-    const error = new Error('Task class not implemented');
-    callback(error);
-  }
-}
