a5b23e46f9eff6a6d0149ee4318d127e8790ac17
Lionel Faber <lionel1704@gmail.com>
2019-03-18 14:27:59 +0530

    fix/web_api_playground: Update dependencies
    - Update dependencies to resolve vulnerabilities
    - Update the gulpfile to support gulp 4

--- safe_web_api_playground/gulpfile.js
+++ safe_web_api_playground/gulpfile.js
@@ -10,52 +10,53 @@ var envfile = require('envfile'),
   source = require('vinyl-source-stream'),
   babelify = require('babelify');
 
-gulp.task('safe-styles', function() {
-  gulp.src([
-    './node_modules/safe_ux_guidelines/scss/**/*.scss',
-    './node_modules/npm-font-open-sans/open-sans.scss',
-    '!./node_modules/safe_ux_guidelines/scss/main.scss'
-  ])
+gulp.task('safe-styles', function(done) {
+  gulp.src('./node_modules/npm-font-open-sans/open-sans.scss')
     .pipe(gulp.dest('./static/scss'));
 
   gulp.src('./static/scss/custom/main.scss')
     .pipe(gulp.dest('./static/scss'));
+    done();
 });
 
-gulp.task('image', function () {
+gulp.task('image', function (done) {
   gulp.src('./static/images/*')
     .pipe(image())
     .pipe(gulp.dest('./build/images'));
 
   gulp.src('./favicon.ico')
     .pipe(gulp.dest('./build'));
+  done();
 });
 
-gulp.task('js-deps', function () {
+gulp.task('js-deps', function (done) {
   gulp.src([
       './node_modules/jquery/dist/jquery.min.js',
       './node_modules/bootstrap/dist/js/bootstrap.min.js'
     ])
     .pipe(concat('deps.js'))
     .pipe(gulp.dest('./build/js'));
+  done();
 });
 
-gulp.task('html', function () {
+gulp.task('html', function (done) {
   gulp.src(
     './index.html'
     )
     .pipe(gulp.dest('./build'));
+  done();
 });
 
-gulp.task('scss', function () {
+gulp.task('scss', function (done) {
   gulp.src([
       './static/scss/main.scss',
     ])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/css'));
+    done();
 });
 
-gulp.task('css-deps', function () {
+gulp.task('css-deps', function (done) {
   gulp.src([
       './node_modules/bootstrap/dist/css/bootstrap.css'
     ])
@@ -65,21 +66,24 @@ gulp.task('css-deps', function () {
       './node_modules/bootstrap/dist/css/bootstrap.css.map'
     ])
     .pipe(gulp.dest('./build/css'));
+  done();
 });
 
-gulp.task('fonts', function () {
+gulp.task('fonts', function (done) {
   gulp.src([
       './static/fonts/**/*.*',
       './node_modules/npm-font-open-sans/fonts/**/*.*'
     ])
     .pipe(gulp.dest('./build/fonts'));
+  done();
 });
 
-gulp.task('codemirror', function () {
+gulp.task('codemirror', function (done) {
   gulp.src([
       './node_modules/codemirror/lib/codemirror.css'
     ])
     .pipe(gulp.dest('./build/css'));
+  done();
 });
 
 gulp.task('js', function () {
@@ -95,10 +99,9 @@ gulp.task('js', function () {
       })
       .pipe(source('client-scripts.js'))
       .pipe(gulp.dest(destinationDirectory))
-
 });
 
-gulp.task('serve', function () {
+gulp.task('serve', function (done) {
   var env = envfile.parseFileSync('.env');
   nodemon({
     script: './index.js',
@@ -106,13 +109,14 @@ gulp.task('serve', function () {
     ignore: ['build/**/*.*', 'static/**/*.*', 'node_modules'],
     tasks: [],
     env: env
-  }).on('restart', function () {
+  }).on('restart', function (event) {
     console.log('server restarted....');
   });
+  done();
 });
 
-gulp.task('watch', function () {
-  watch(['./static/js/*.js', './static/js/**/*.js'], function () {
+gulp.task('watch', function (done) {
+  watch (['./static/js/*.js', './static/js/**/*.js'], function () {
     gulp.start('js');
   });
 
@@ -123,6 +127,7 @@ gulp.task('watch', function () {
   watch('./index.html', function () {
     gulp.start('html');
   });
+  done();
 });
 
-gulp.task('default', ['codemirror', 'js-deps', 'html', 'scss', 'css-deps', 'js', 'watch', 'serve', 'fonts', 'image']);
+gulp.task('default', gulp.series('codemirror', 'js-deps', 'html', 'scss', 'css-deps', 'js', 'watch', 'serve', 'fonts', 'image'));
